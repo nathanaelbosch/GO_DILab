ae448db852a3d99fcbe5300684d92b1dfc23bf52
Nathanael Bosch <nathanael.bosch@gmail.com>
2017-10-20 09:02:18 +0200

    demo: replay from sgf file

---
+++ examples/play_from_sgf.py
@@ -0,0 +1,24 @@
+"""Read a single SGF file and recreate the game"""
+import sgf
+import sys
+
+sys.path.append('.')
+from go import GO_game
+
+
+file = 'data/unpacked/2017/09/08/338554.sgf'
+with open(file, 'r') as f:
+    content = f.read()
+    collection = sgf.parse(content)
+
+# Assume the sgf file contains one game
+game_tree = collection.children[0]
+n_0 = game_tree.nodes[0]
+n_0.properties      # Contains the initial game setup
+
+game = GO_game(n_0.properties, show_each_turn=True)
+for node in game_tree.nodes[1:]:
+    if 'W' in node.properties.keys():
+        game.w(node.properties['W'][0])
+    if 'B' in node.properties.keys():
+        game.b(node.properties['B'][0])
