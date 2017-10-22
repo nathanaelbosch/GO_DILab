ff7c609159eaa053477723fc9e6e1e043fc44068
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2017-10-22 16:15:41 +0200

    apply rename, remove unused game array

--- sgf2games.py
+++ sgf2games.py
@@ -1,5 +1,5 @@
 import sgf
-from go import GO_game
+from go import Game
 import os
 import re
 
@@ -20,14 +20,13 @@ for file in filepaths:
     # Assume the sgf file contains one game
     game_tree = collection.children[0]
     n_0 = game_tree.nodes[0]
-    n_0.properties      # Contains the initial game setup
+    # n_0.properties contains the initial game setup
     game_id = n_0.properties['GN'][0]
     myfilepath = 'data/myformat/'+game_id+'.csv'
     if os.path.isfile(myfilepath):
         os.remove(myfilepath)
 
-    engine = GO_game(n_0.properties, show_each_turn=True)
-    game = []
+    engine = Game(n_0.properties, show_each_turn=True)
     for n in game_tree.nodes[1:]:
         props = n.properties
         # print(props)
@@ -36,5 +35,4 @@ for file in filepaths:
         if 'B' in props.keys():
             engine.b(props['B'][0])
         # print(engine)
-        game.append(engine.board)
         engine.board2file(myfilepath, 'a')
