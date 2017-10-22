41394ad540b511afec1b4316118ba277d94b9939
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2017-10-22 18:29:53 +0200

    define proper main() in run script

--- src/run.py
+++ src/run.py
@@ -1,8 +1,15 @@
 from src import Game, ConsoleView, HumanConsolePlayer, RandomBotPlayer, GameController
 
-game = Game()
-view = ConsoleView(game)
-player1 = HumanConsolePlayer("PersonA", "w", game)
-player2 = RandomBotPlayer("RandomBot", "b", game)
-gameController = GameController(game, view, player1, player2)
-gameController.start()
+
+def main():
+    game = Game()
+    view = ConsoleView(game)
+    player1 = HumanConsolePlayer("PersonA", "w", game)
+    # player2 = HumanConsolePlayer("PersonB", "b", game)
+    player2 = RandomBotPlayer("RandomBot", "b", game)
+    game_controller = GameController(game, view, player1, player2)
+    game_controller.start()
+
+
+if __name__ == '__main__':
+    main()
