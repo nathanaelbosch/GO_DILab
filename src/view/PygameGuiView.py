b7bc4aaf082e4121ee0d0755c13c83029ece0e4c
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2017-10-26 23:32:36 +0200

    create draw_board method and use for updating, move class variables upwards

--- src/view/PygameGuiView.py
+++ src/view/PygameGuiView.py
@@ -5,6 +5,13 @@ from src.view import View
 from src.model.Game import BLACK
 from src.model.Game import WHITE
 
+brown = (165, 42, 42)
+black = (0, 0, 0)
+white = (255, 255, 255)
+size = (500, 500)
+width = 20
+height = 20
+
 
 class PygameGuiView(View):
 
@@ -15,19 +22,9 @@ class PygameGuiView(View):
     def open(self, game_controller):
         pygame.init()
         self.running = True
-        black = (0, 0, 0)
-        white = (255, 255, 255)
-        size = (550, 550)
-        brown = (165, 42, 42)
-        width = 20
-        height = 20
         self.screen = pygame.display.set_mode(size)
-        self.screen.fill(brown)
-        for i in range(1, 10):
-            pygame.draw.line(self.screen, black, [50, i * 50], [450, i * 50], 1)
-            pygame.draw.line(self.screen, black, [50 * i, 50], [50 * i, 450], 1)
         pygame.display.set_caption('Go')
-
+        self.draw_board()
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.MOUSEBUTTONUP:
@@ -43,6 +40,7 @@ class PygameGuiView(View):
         pass  # TODO
 
     def show_player_turn_end(self, name):
+        self.draw_board()
         b = self.game.board.copy()
         b[b == BLACK] = 2
         b[b == WHITE] = 3
@@ -50,6 +48,7 @@ class PygameGuiView(View):
         white_rows, white_cols = np.where(b == 3)
         # This method does not really make sense because it draws circles over previously existing ones.
         # Will update once I get the hang of the code.
+        # make the circles look nicer -> antialiasing, see stackoverflow.com/a/26774279/2474159 TODO
         for i in range(0, len(black_rows)):
             pygame.draw.circle(self.screen, (0, 0, 0), (50 + 50 * black_cols[i], 50 + black_rows[i] * 50), 20, 0)
         for i in range(0, len(white_rows)):
@@ -57,3 +56,9 @@ class PygameGuiView(View):
 
     def show_error(self, msg):
         pass
+
+    def draw_board(self):
+        self.screen.fill(brown)
+        for i in range(1, 10):
+            pygame.draw.line(self.screen, black, [50, i * 50], [450, i * 50], 1)
+            pygame.draw.line(self.screen, black, [50 * i, 50], [50 * i, 450], 1)
