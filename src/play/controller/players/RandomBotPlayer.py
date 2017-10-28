dbb49e586b064cde3e4b963ba1c764c3ebac926d
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2017-10-29 01:35:06 +0200

    let the RandomBotPlayer wait a little bit

--- src/play/controller/players/RandomBotPlayer.py
+++ src/play/controller/players/RandomBotPlayer.py
@@ -1,5 +1,6 @@
 from src.play import Player
 import random as rn
+import time
 
 
 class RandomBotPlayer(Player):
@@ -10,4 +11,5 @@ class RandomBotPlayer(Player):
     def get_move(self):
         playable_locations = self.game.get_playable_locations(self.color)
         random_choice = rn.choice(playable_locations)
+        time.sleep(0.3)
         return random_choice
