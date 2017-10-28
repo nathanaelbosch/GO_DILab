8f53bef443820474f815d592388c3811a0c4b0aa
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2017-10-28 23:59:58 +0200

    fix bug in str2move()

--- src/play/utils/Utils.py
+++ src/play/utils/Utils.py
@@ -7,15 +7,15 @@ def call_method_on_each(arr, method, *args):  # via stackoverflow.com/a/2682075/
         getattr(obj, method)(*args)
 
 
-def str2move(loc: str, board_size) -> Move:
-    if not str:
+def str2move(move_str: str, board_size: int) -> Move:
+    if not move_str:
         return Move(is_pass=True)
-    col = _chr2ord(loc[0], board_size)
-    row = _chr2ord(loc[1], board_size)
+    col = _chr2ord(move_str[0], board_size)
+    row = _chr2ord(move_str[1], board_size)
     return Move(col, row)
 
 
-def _chr2ord(c: str, board_size) -> int:
+def _chr2ord(c: str, board_size: int) -> int:
     idx = ord(c) - ord('a')
     if idx < 0 or idx >= board_size:
         raise InvalidMove_Error(
