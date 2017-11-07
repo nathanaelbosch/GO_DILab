74b9cdf46e0d20a9b2e270fea05e1a1435517f34
Nathanael Bosch <nathanael.bosch@gmail.com>
2017-11-07 17:44:05 +0100

    added ML, made default logging level ERROR

--- src/play/model/Game.py
+++ src/play/model/Game.py
@@ -29,7 +29,7 @@ logging.basicConfig(
     format='%(levelname)s:%(name)s:%(message)s',
     datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.ERROR)
 
 
 class GO_Error(Exception):
