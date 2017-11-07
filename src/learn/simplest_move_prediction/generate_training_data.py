1120098ffb140104a345ae0f57088a2b6e71a255
Nathanael Bosch <nathanael.bosch@gmail.com>
2017-11-07 12:27:22 +0100

    how about now?

--- src/learn/simplest_move_prediction/generate_training_data.py
+++ src/learn/simplest_move_prediction/generate_training_data.py
@@ -9,7 +9,7 @@ from src.play.model.Game import Game
 from src.play.utils.Move import Move
 
 
-OUT = 'data/training_data/simplest_move_prediction/train.csv'
+OUT_PATH = 'data/training_data/simplest_move_prediction/'
 
 
 def list_all_sgf_files(dir):
@@ -42,56 +42,57 @@ def replay_game(file, func):
     # This all only works if the SGF contains only one game
     game_tree = collection.children[0]
     n_0 = game_tree.nodes[0]
+    game_id = n_0.properties['GN'][0]
 
     game = Game(n_0.properties)
     for n in game_tree.nodes[1:]:
         player_color = list(n.properties.keys())[0]
         move = Move.from_sgf(str(n.properties[player_color][0]))
         game.play(move, player_color.lower())
-        func(game, move, player_color.lower())
+        func(game_id+'.csv', game, move, player_color.lower())
 
 
 def get_string(game, move, player):
-    def board_to_csv(board):
-        ls = board.tolist()
+    ls = game.board.tolist()
+    ls = [str(entry) for row in ls for entry in row]
+    board_string = ';'.join(ls)
+
+    if move.is_pass:
+        # s = ';'.join(['0']*(9*9)+['1'])
+        move_string = ';'.join(['0']*(9*9))
+    else:
+        new_game = Game()
+        new_game.play(move, player.lower())
+        ls = new_game.board.tolist()
         ls = [str(entry) for row in ls for entry in row]
-        s = ';'.join(ls)
-        return s
+        # s = ';'.join(ls+['0'])
+        move_string = ';'.join(ls)
 
-    def move_to_csv(move, player):
-        if move.is_pass:
-            # s = ';'.join(['0']*(9*9)+['1'])
-            s = ';'.join(['0']*(9*9))
-            return s
-        else:
-            new_game = Game()
-            new_game.play(move, player.lower())
-            ls = new_game.board.tolist()
-            ls = [str(entry) for row in ls for entry in row]
-            # s = ';'.join(ls+['0'])
-            s = ';'.join(ls)
-            return s
-
-    b = board_to_csv(game.board)
-    m = move_to_csv(move, player)
-    s = b+';'+m
-    # print(s)
+    s = board_string + ';' + move_string
     return s
 
 
-def to_training_file(*args, **kwargs):
-    with open(OUT, 'a') as f:
+def to_training_file(out, *args, **kwargs):
+    out = os.path.join(OUT_PATH, out)
+    with open(out, 'a') as f:
         f.write(get_string(*args, **kwargs))
         f.write('\n')
 
 
+def foo(file):
+    try:
+        replay_game(file, to_training_file)
+    except Exception:
+        return
+
+
 def main():
     files = list_all_sgf_files('data')
-    files = rn.sample(files, 1000)
+    # files = rn.sample(files, 1000)
 
     import multiprocessing
     pool = multiprocessing.Pool()
-    pool.map(lambda x: replay_game(x, to_training_file), files)
+    pool.map(foo, files)
 
 
 if __name__ == '__main__':
