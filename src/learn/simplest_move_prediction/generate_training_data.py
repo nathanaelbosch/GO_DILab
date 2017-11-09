728a64152cd0d50c84b47f213954eafe4f98c398
Nathanael Bosch <nathanael.bosch@gmail.com>
2017-11-09 17:40:39 +0100

    new approach: single input file; create numpy arrays; save to single file

--- src/learn/simplest_move_prediction/generate_training_data.py
+++ src/learn/simplest_move_prediction/generate_training_data.py
@@ -5,6 +5,7 @@ On each move we can create a new datapoint, or even 8 adding symmetries!
 import os
 import sgf
 import random as rn
+import numpy as np
 from src.play.model.Game import Game
 from src.play.utils.Move import Move
 
@@ -12,32 +13,24 @@ from src.play.utils.Move import Move
 OUT_PATH = 'data/training_data/simplest_move_prediction/'
 
 
-def list_all_sgf_files(dir):
-    """List all sgf-files in a dir
-
-    Recursively explores a path and returns the filepaths
-    for all files ending in .sgf
-    """
-    root_dir = os.path.abspath(dir)
-    sgf_files = []
-    for root, sub_dirs, files in os.walk(root_dir):
-        for file in files:
-            path = os.path.join(root, file)
-            name, extension = os.path.splitext(path)
-            if extension == '.sgf':
-                sgf_files.append(path)
-    return sgf_files
+def error_resistance(funct):
+    def asdf(*args, **kwargs):
+        try:
+            return funct(*args, **kwargs)
+        except Exception as e:
+            # raise e
+            return
+    return asdf
 
 
-def replay_game(file, func):
+@error_resistance
+def replay_game(sgf_line, func):
     """Simply recreate a game from a sgf file
 
     More of a proof-of-concept or example than really a necessary function.
     We will use some modified version of this to create the training data.
     """
-    with open(file, 'r') as f:
-        content = f.read()
-        collection = sgf.parse(content)
+    collection = sgf.parse(sgf_line)
 
     # This all only works if the SGF contains only one game
     game_tree = collection.children[0]
@@ -45,54 +38,44 @@ def replay_game(file, func):
     game_id = n_0.properties['GN'][0]
 
     game = Game(n_0.properties)
+    out = []
     for n in game_tree.nodes[1:]:
         player_color = list(n.properties.keys())[0]
         move = Move.from_sgf(str(n.properties[player_color][0]))
         game.play(move, player_color.lower())
-        func(game_id+'.csv', game, move, player_color.lower())
+        out.append(func(game, move, player_color.lower()))
+    out = np.stack(out)
+    print(out.shape)
+    return out
 
 
-def get_string(game, move, player):
-    ls = game.board.tolist()
-    ls = [str(entry) for row in ls for entry in row]
-    board_string = ';'.join(ls)
-
-    if move.is_pass:
-        # s = ';'.join(['0']*(9*9)+['1'])
-        move_string = ';'.join(['0']*(9*9))
-    else:
-        new_game = Game()
-        new_game.play(move, player.lower())
-        ls = new_game.board.tolist()
-        ls = [str(entry) for row in ls for entry in row]
-        # s = ';'.join(ls+['0'])
-        move_string = ';'.join(ls)
-
-    s = board_string + ';' + move_string
-    return s
-
-
-def to_training_file(out, *args, **kwargs):
-    out = os.path.join(OUT_PATH, out)
-    with open(out, 'a') as f:
-        f.write(get_string(*args, **kwargs))
-        f.write('\n')
-
-
-def foo(file):
-    try:
-        replay_game(file, to_training_file)
-    except Exception:
-        return
+def to_numpy(game, move, player):
+    b = game.board
+    board_vect = b.reshape(1, b.shape[0]*b.shape[1])
+    new_game = Game()
+    new_game.play(move, player.lower())
+    b = new_game.board
+    move_vect = b.reshape(1, b.shape[0]*b.shape[1])
+    color = 1 if player.lower == 'b' else -1
+    vect = np.append(color, (board_vect, move_vect))
+    # print(vect)
+    return vect
 
 
 def main():
-    files = list_all_sgf_files('data')
-    files = rn.sample(files, 1000)
+    file = 'data/full_file.txt'
+    with open(file, 'r') as f:
+        lines = f.readlines()
+    lines = rn.sample(lines, 10)
 
-    import multiprocessing
-    pool = multiprocessing.Pool()
-    pool.map(foo, files)
+    # import multiprocessing
+    # pool = multiprocessing.Pool()
+    # data = pool.map(foo, lines)
+
+    data = map(lambda x: replay_game(x, to_numpy), lines)
+    data = [d for d in data if d is not None]
+    print(data[0].shape)
+    print(data[0][0].shape)
 
 
 if __name__ == '__main__':
