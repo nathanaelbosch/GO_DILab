e3e6313a99125c0fa9885c39ad785073298327bd
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2017-11-13 22:02:31 +0100

    set project_dir to allow calling script from anywhere

--- src/client/GTPplayer.py
+++ src/client/GTPplayer.py
@@ -1,5 +1,8 @@
 import sys
 import random
+from os.path import dirname, abspath
+project_dir = dirname(dirname(dirname(abspath(__file__))))
+sys.path.append(project_dir)
 from src.play.model.Game import InvalidMove_Error, Game
 from src.play.utils.Move import Move
 
