abe487be2f1de2db0e08baa591aa630f8fa7e8df
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2017-11-13 21:11:48 +0100

    change order to match order in 6.1 of specification
    www.lysator.liu.se/~gunnar/gtp/gtp2-spec-draft2/gtp2-spec.html#SECTION00071000000000000000

--- src/client/GTPplayer.py
+++ src/client/GTPplayer.py
@@ -12,18 +12,31 @@ class GTPplayer:
         self.out = sys.stdout
 
         self.gtp_commands = {
-            self.genmove.__name__: self.genmove,
-            self.play.__name__: self.play,
-            self.list_commands.__name__: self.list_commands,
-            self.known_command.__name__: self.known_command,
-            self.quit.__name__: self.quit,
             self.protocol_version.__name__: self.protocol_version,
-            self.version.__name__: self.version,
             self.name.__name__: self.name,
+            self.version.__name__: self.version,
+            self.known_command.__name__: self.known_command,
+            self.list_commands.__name__: self.list_commands,
+            self.quit.__name__: self.quit,
             self.boardsize.__name__: self.boardsize,
             self.clear_board.__name__: self.clear_board,
             self.komi.__name__: self.komi,
+            self.play.__name__: self.play,
+            self.genmove.__name__: self.genmove,
         }
+
+    def run(self):
+        while True:
+            stdin_line = sys.stdin.readline().strip()
+            if len(stdin_line) == 0:  # ignore empty lines
+                continue
+            # print('reading input: ' + stdin_line)
+            parts = stdin_line.split(' ')
+            command = parts[0]
+            if command not in self.gtp_commands:
+                self.send_failure_response('command unknown: ' + stdin_line)
+                continue
+            self.gtp_commands[command](parts[1:])
 
     def send_success_response(self, message=''):
         # following lysator.liu.se/~gunnar/gtp/gtp2-spec-draft2/gtp2-spec.html#SECTION00044000000000000000
@@ -45,33 +58,17 @@ class GTPplayer:
             return col
         return None
 
-    def genmove(self, args):
-        if len(args) == 0:
-            self.send_failure_response('no color provided')
-            return
-        color = self.validate_color(args[0])
-        if color is None:
-            self.send_failure_response('invalid color ' + args[0])
-            return
-        random_move = rn.choice(self.game.get_playable_locations(color))
-        self.game.play(random_move, color)  # or does the controller tell us to do this?
-        self.send_success_response(random_move.to_gtp())
+    # ---------- COMMAND METHODS ----------
 
-    def play(self, args):
-        if len(args) < 2:
-            self.send_failure_response('no color and/or move provided')
-            return
-        color = self.validate_color(args[0])
-        if color is None:
-            self.send_failure_response('invalid color ' + args[0])
-            return
-        gtp_move = args[1]
-        move = Move().from_gtp(gtp_move)
-        self.game.play(move, color)
-        self.send_success_response()
+    def protocol_version(self, args):
+        # version of the GTP specification lysator.liu.se/~gunnar/gtp/gtp2-spec-draft2/gtp2-spec.html
+        self.send_success_response('2')
 
-    def list_commands(self, args):
-        self.send_success_response('\n'+'\n'.join(list(self.gtp_commands.keys())))
+    def name(self, args):
+        self.send_success_response('Hikaru - Random Bot')
+
+    def version(self, args):
+        self.send_success_response('1.0')
 
     def known_command(self, args):
         if len(args) == 0:
@@ -79,20 +76,13 @@ class GTPplayer:
         else:
             self.send_success_response(str(args[0] in self.gtp_commands))
 
+    def list_commands(self, args):
+        self.send_success_response('\n'+'\n'.join(list(self.gtp_commands.keys())))
+
     def quit(self, args):
         self.send_success_response()
         self.out.close()
         exit(0)
-
-    def protocol_version(self, args):
-        # version of the GTP specification lysator.liu.se/~gunnar/gtp/gtp2-spec-draft2/gtp2-spec.html
-        self.send_success_response('2')
-
-    def version(self, args):
-        self.send_success_response('1.0')
-
-    def name(self, args):
-        self.send_success_response('Hikaru - Random Bot')
 
     def boardsize(self, args):
         if len(args) == 0:
@@ -120,18 +110,30 @@ class GTPplayer:
         except ValueError:
             self.send_failure_response('not an acceptable value: ' + args[0])
 
-    def run(self):
-        while True:
-            stdin_line = sys.stdin.readline().strip()
-            if len(stdin_line) == 0:  # ignore empty lines
-                continue
-            # print('reading input: ' + stdin_line)
-            parts = stdin_line.split(' ')
-            command = parts[0]
-            if command not in self.gtp_commands:
-                self.send_failure_response('command unknown: ' + stdin_line)
-                continue
-            self.gtp_commands[command](parts[1:])
+    def play(self, args):
+        if len(args) < 2:
+            self.send_failure_response('no color and/or move provided')
+            return
+        color = self.validate_color(args[0])
+        if color is None:
+            self.send_failure_response('invalid color ' + args[0])
+            return
+        gtp_move = args[1]
+        move = Move().from_gtp(gtp_move)
+        self.game.play(move, color)
+        self.send_success_response()
+
+    def genmove(self, args):
+        if len(args) == 0:
+            self.send_failure_response('no color provided')
+            return
+        color = self.validate_color(args[0])
+        if color is None:
+            self.send_failure_response('invalid color ' + args[0])
+            return
+        random_move = rn.choice(self.game.get_playable_locations(color))
+        self.game.play(random_move, color)  # or does the controller tell us to do this?
+        self.send_success_response(random_move.to_gtp())
 
 
 def main():
