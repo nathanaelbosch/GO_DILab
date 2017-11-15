8c1f53ab9aa631f3283ba0bfb7ba594a63f211e4
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2017-11-15 21:31:00 +0100

    make the signature more precise

--- bots/RandomBot.py
+++ bots/RandomBot.py
@@ -1,10 +1,12 @@
 import random
 
+import Move
+
 
 class RandomBot:
 
     def __init__(self, game):
         self.game = game
 
-    def genmove(self, color):
+    def genmove(self, color) -> Move:
         return random.choice(self.game.get_playable_locations(color))
