14a5973cb11f3c45871cee0084ce2ac3f3b47ebd
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2017-11-22 15:43:51 +0100

    add run script for conveniently starting a GTPcontroller

---
+++ src/play/run.py
@@ -0,0 +1,21 @@
+from os.path import dirname, abspath
+
+import sys
+
+project_dir = dirname(dirname(dirname(abspath(__file__))))
+sys.path.append(project_dir)
+
+from src.play.controller.GTPcontroller import GTPcontroller
+from src.play.controller.bots import RandomBot, SimplestNNBot
+
+
+def main():
+    GTPcontroller(
+        RandomBot.__name__,
+        SimplestNNBot.__name__,
+        # HumanConsole.__name__,
+    ).run()
+
+
+if __name__ == '__main__':
+    main()
