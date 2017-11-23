e73c648c1a3bd0dec532afd9273fe4a42bf6440e
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2017-11-23 20:42:36 +0100

    resurrect @nathbo's cool argparsing setup

--- src/play/run.py
+++ src/play/run.py
@@ -1,6 +1,7 @@
 import logging
 import sys
 from os.path import dirname, abspath
+import argparse
 
 from src.play.view.PygameView import PygameView
 
@@ -16,17 +17,46 @@ from src.play.controller.bots.RandomGroupingBot import RandomGroupingBot
 from src.learn.dev_nath.SimplestNNBot import SimplestNNBot
 
 
-def main():
-    # if you don't want logfiles: change the logging-level to something more critical than INFO (e.g. WARNING)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--no-gui',
+        help='No GUI')
+    parser.add_argument(
+        '-p1', '--player1',
+        help='Player 1 - black - options: "human", "random", "random_grouping" or "dev_nn_nath"',
+        default='random')
+    parser.add_argument(
+        '-p2', '--player2',
+        help='Player 2 - white - options: "human", "random", "random_grouping" or "dev_nn_nath"',
+        default='random')
+    return parser.parse_args()
 
-    controller = GTPcontroller(
-        RandomBot.__name__,
-        RandomGroupingBot.__name__,
-        logging.INFO,
-    )
-    view = PygameView(controller)
+
+def main():
+    args = parse_args()
+
+    if args.no_gui:
+        if args.player1 == 'human':
+            args.player1 = 'human_console'
+        if args.player2 == 'human':
+            args.player2 = 'human_console'
+
+    player_types = {
+        'human': HumanGui,
+        'human_console': HumanConsole,
+        'random': RandomBot,
+        'random_grouping': RandomGroupingBot,
+        'dev_nn_nath': SimplestNNBot,
+    }
+    player1type = player_types[args.player1].__name__
+    player2type = player_types[args.player2].__name__
+
+    # if you don't want logfiles: change the logging-level to something more critical than INFO (e.g. WARNING)
+    controller = GTPcontroller(player1type, player2type, logging.INFO)
     controller.start()
-    view.open()
+    if not args.no_gui:
+        PygameView(controller).open()
 
 
 if __name__ == '__main__':
