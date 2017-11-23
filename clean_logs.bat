68901b976faf59b20b9731457eca5f8a9948a8b3
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2017-11-23 18:41:32 +0100

    add clean_log scripts

---
+++ clean_logs.bat
@@ -0,0 +1,3 @@
+rd /S /Q .\src\play\logs
+mkdir .\src\play\logs
+copy /y nul .\src\play\logs\.gitkeep
---
+++ clean_logs.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+rm -r ./src/play/logs
+mkdir ./src/play/logs
+touch ./src/play/logs/.gitkeep
