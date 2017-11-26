01483f2249e5772f510f932b4eeeab6d837edc4f
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2017-11-26 21:22:10 +0100

    experimental: write next move at end of line, E, B, W instead of float

--- src/learn/dev_ben/generate_training_data.py
+++ src/learn/dev_ben/generate_training_data.py
@@ -4,9 +4,9 @@ from os.path import dirname, abspath
 from src.play.model.Board import Board
 
 size = 9
-EMPTY_val = 0  # 0.45
-BLACK_val = 1  # -1.35
-WHITE_val = -1  # 1.05
+EMPTY_val = 'E'  # 0.45
+BLACK_val = 'B'  # -1.35
+WHITE_val = 'W'  # 1.05
 
 data_dir = os.path.join(dirname(dirname(dirname(dirname(abspath(__file__))))), 'data')
 sgf_files = [
@@ -29,7 +29,7 @@ for path in sgf_files:
     # see SGF properties here: www.red-bean.com/sgf/properties.html
 
     board = Board([[EMPTY_val] * size] * size)
-    training_data_file.write(board.matrix2csv() + '\n')
+    lines = [board.matrix2csv()]
 
     for move in moves:
         keys = move.properties.keys()
@@ -38,9 +38,20 @@ for path in sgf_files:
         # can't rely on the order in keys(), apparently must extract it like this
         player_color = 'B' if 'B' in move.properties.keys() else 'W'
         sgf_move = move.properties[player_color][0]
+        append_to_previous_line = player_color + ':'
         if len(sgf_move) is 2:  # otherwise its a pass
             loc = ord(sgf_move[1]) - ord('a'), ord(sgf_move[0]) - ord('a')
             player_val = BLACK_val if player_color == 'B' else WHITE_val
             opponent_val = WHITE_val if player_color == 'B' else BLACK_val
             board.place_stone_and_capture_if_applicable(loc, player_val, opponent_val, EMPTY_val)
-        training_data_file.write(board.matrix2csv() + '\n')
+            append_to_previous_line += str(loc[0] * size + loc[1])
+        else:
+            append_to_previous_line += 'PASS'
+
+        lines[len(lines) - 1] += ';' + append_to_previous_line
+        lines.append(board.matrix2csv())
+    lines[len(lines) - 1] += ';END'
+
+    for line in lines:
+        training_data_file.write(line + '\n')
+    training_data_file.close()
