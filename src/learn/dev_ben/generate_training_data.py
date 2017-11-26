db1f0556f72eb84e4273ff8925494de81bf21898
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2017-11-26 19:33:33 +0100

    rename paths / meta not needed

--- src/learn/dev_ben/generate_training_data.py
+++ src/learn/dev_ben/generate_training_data.py
@@ -10,7 +10,7 @@ BLACK_val = 1  # -1.35
 WHITE_val = -1  # 1.05
 
 data_dir = os.path.join(dirname(dirname(dirname(dirname(abspath(__file__))))), 'data')
-paths = [
+sgf_files = [
     os.path.join(data_dir, 'game_57083.sgf'),
     os.path.join(data_dir, 'game_100672.sgf'),
 ]
@@ -19,15 +19,15 @@ training_data_dir = os.path.join(data_dir, 'training_data')
 if not os.path.exists(training_data_dir):  # create the folder if it does not exist yet
     os.makedirs(training_data_dir)
 training_data_file = open(
-    os.path.join(training_data_dir, str(len(paths)) + '_games_' + strftime('%d-%m-%Y_%H-%M-%S') + '.csv'), 'w')
+    os.path.join(training_data_dir, str(len(sgf_files)) + '_games_' + strftime('%d-%m-%Y_%H-%M-%S') + '.csv'), 'w')
 
-for path in paths:
+for path in sgf_files:
     sgf_file = open(path, 'r')
     training_data_file.write(os.path.basename(path) + '\n')
     collection = sgf.parse(sgf_file.read())
     game_tree = collection.children[0]
-    meta = game_tree.nodes[0].properties
     moves = game_tree.nodes[1:]
+    # meta = game_tree.nodes[0].properties
     # see SGF properties here: www.red-bean.com/sgf/properties.html
 
     board = Board([[EMPTY_val] * size] * size)
