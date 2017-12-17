b8a16d821cce1d276b23cadaf1f2df68a2bf7277
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2017-12-17 14:13:15 +0100

    replace use_scipy() logic with in_pyinstaller_mode()

--- src/learn/BaseNNBot.py
+++ src/learn/BaseNNBot.py
@@ -11,9 +11,15 @@ from src.play.model.Move import Move
 class BaseNNBot(ABC):
 
     def __init__(self):
-        Utils.set_keras_backend('theano')
+        if Utils.in_pyinstaller_mode():
+            # path to root dir, expects data to be added like this when calling pyinstaller:
+            # --add-data ".\src\learn\[...]\model.json;."
+            # the dot means: place it in root dir
+            model_files_dir = sys._MEIPASS
+        else:  # if not in pyinstaller mode, use theano, because it works with pygame, tensorflow doesn't
+            Utils.set_keras_backend('theano')
+            model_files_dir = dirname(self.get_path_to_self())
         from keras.models import model_from_json
-        model_files_dir = dirname(self.get_path_to_self())  # sys._MEIPASS, for use with pyinstaller
         model_architecture_path = os.path.join(model_files_dir, 'model_architecture.json')
         if not os.path.isfile(model_architecture_path):
             print('model architecture not found: ' + model_architecture_path)
