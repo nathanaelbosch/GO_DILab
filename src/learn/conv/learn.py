c09f4607205b3319d66d67b52da5590880b92f88
Nathanael Bosch <nathanael.bosch@tum.de>
2018-01-10 09:38:15 +0100

    running version - So far best I could do was ~16% on the testset

--- src/learn/conv/learn.py
+++ src/learn/conv/learn.py
@@ -49,6 +49,59 @@ class ConvNet(torch.nn.Module):
         self.softmax = torch.nn.Softmax(dim=1)
 
     def forward(self, x):
+        return self._forward(x)
+
+    def _forward_with_symmetries(self, x):
+        """DISREGARD FOR NOW
+
+        Compute symmetries at runtime, evaluate the model for each, then
+        average over the predictions and transform symmetries back
+        """
+        _, c, w, h = x.size()
+        x_np = x.data.numpy()
+        boards = x_np
+
+        # Create symmetries
+        boards_90 = np.rot90(boards, axes=(2, 3))
+        boards_180 = np.rot90(boards, k=2, axes=(2, 3))
+        boards_270 = np.rot90(boards, k=3, axes=(2, 3))
+        boards_flipped = np.fliplr(boards)
+        boards_flipped_90 = np.rot90(np.fliplr(boards), axes=(2, 3))
+        boards_flipped_180 = np.rot90(np.fliplr(boards), k=2, axes=(2, 3))
+        boards_flipped_270 = np.rot90(np.fliplr(boards), k=3, axes=(2, 3))
+
+        # Symmetries to pytorch
+        boards = Variable(torch.from_numpy(boards.copy()))
+        boards_90 = Variable(torch.from_numpy(boards_90.copy()))
+        boards_180 = Variable(torch.from_numpy(boards_180.copy()))
+        boards_270 = Variable(torch.from_numpy(boards_270.copy()))
+        boards_flipped = Variable(torch.from_numpy(boards_flipped.copy()))
+        boards_flipped_90 = Variable(torch.from_numpy(boards_flipped_90.copy()))
+        boards_flipped_180 = Variable(torch.from_numpy(boards_flipped_180.copy()))
+        boards_flipped_270 = Variable(torch.from_numpy(boards_flipped_270.copy()))
+
+        if x.is_cuda:
+            boards.cuda()
+            boards_90.cuda()
+            boards_180.cuda()
+            boards_270.cuda()
+            boards_flipped.cuda()
+            boards_flipped_90.cuda()
+            boards_flipped_180.cuda()
+            boards_flipped_270.cuda()
+
+        out = self._forward(boards)
+        out_90 = self._forward(boards_90)
+        out_180 = self._forward(boards_180)
+        out_270 = self._forward(boards_270)
+        out_flipped = self._forward(boards_flipped)
+        out_flipped_90 = self._forward(boards_flipped_90)
+        out_flipped_180 = self._forward(boards_flipped_180)
+        out_flipped_270 = self._forward(boards_flipped_270)
+
+        return None
+
+    def _forward(self, x):
         x = self.start_conv(x)
         x = self.start_relu(x)
         for conv, relu in zip(self.mid_convs, self.relus):
@@ -75,7 +128,7 @@ class Learn():
         self.data_retrieval_command = '''
             SELECT *
             FROM elo_ordered_games
-            -- ORDER BY RANDOM()
+            ORDER BY RANDOM()
             LIMIT ?'''
         self.db = sqlite3.connect(DB_PATH)
 
@@ -100,22 +153,18 @@ class Learn():
         y = y.values
         X = utils.encode_board(boards, data['color'])
         X = X.reshape(-1, 3, 9, 9).astype(float)
+
+        X = X[~passes]
+        y = y[~passes]
         assert X.shape[0] == y.shape[0], 'Something went wrong with the shapes'
         print('X.shape:', X.shape, 'X.dtype:', X.dtype)
         print('y.shape:', y.shape, 'y.dtype:', y.dtype)
 
-        X = X[~passes]
-        y = y[~passes]
-
-        X = torch.from_numpy(X).float()
-        y = torch.from_numpy(y)
-        print('X.size:', X.size(), 'X.dtype:', X.type())
-        print('y.size:', y.size(), 'y.dtype:', y.type())
-
         return X, y
 
-    def train_model(self, X, y):
-        model = ConvNet(X.size(), y.size())
+    def train_model(self):
+        model = self.model
+        X, y = self.X_train, self.y_train
         criterion = torch.nn.CrossEntropyLoss()
         optimizer = torch.optim.Adam(model.parameters())
 
@@ -140,6 +189,7 @@ class Learn():
                     labels = Variable(labels.cuda())
                 else:
                     inputs, labels = Variable(inputs), Variable(labels)
+
                 optimizer.zero_grad()
 
                 # print('Inputs in cuda:', inputs.is_cuda)
@@ -162,24 +212,99 @@ class Learn():
                     running_loss = 0.0
                     correct = 0
                     total = 0
+            self.test_model()
         print('Finished Training')
+
+    def to_pytorch(self, X_train, y_train, X_test, y_test):
+        return (
+            torch.from_numpy(X_train).float(),
+            torch.from_numpy(y_train),
+            torch.from_numpy(X_test).float(),
+            torch.from_numpy(y_test))
+
+    def train_test_split(self, X, y, p=0.9):
+        msk = np.random.rand(X.shape[0]) < 0.9
+        return X[msk], y[msk], X[~msk], y[~msk]
+
+    def test_model(self):
+        dataset = Data.TensorDataset(
+            data_tensor=self.X_test, target_tensor=self.y_test)
+        test_loader = Data.DataLoader(
+            dataset, batch_size=self.batch_size, shuffle=True)
+        correct = 0
+        total = 0
+        for data in test_loader:
+            inputs, labels = data
+            if torch.cuda.is_available():
+                inputs = Variable(inputs.cuda())
+                labels = Variable(labels.cuda())
+            else:
+                inputs, labels = Variable(inputs), Variable(labels)
+            outputs = self.model(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels.data).sum()
+
+        print('Test Accuracy: {:.3f}%'.format(
+            100 * correct / total))
 
     def run(self):
         data = self.get_data()
         X, y = self.format_data(data)
 
-        self.train_model(X, y)
+        X_train, y_train, X_test, y_test = self.train_test_split(X, y)
+        X_train, y_train, X_test, y_test = self.to_pytorch(
+            X_train, y_train, X_test, y_test)
+
+        self.X_train, self.y_train, self.X_test, self.y_test = (
+            X_train, y_train, X_test, y_test)
+        self.model = ConvNet(X_train.size(), y_test.size())
+        self.train_model()
+        self.test_model()
+        print('Save model')
+        torch.save(self.model, 'src/learn/conv/convnet.pt')
+
+
+def test():
+    _l = Learn(
+        training_size=100,
+        batch_size=10,
+        print_every=1,
+        epochs=3,
+        conv_depth=1,
+    )
+    _l.data_retrieval_command = '''
+        SELECT *
+        FROM elo_ordered_games
+        LIMIT ?'''
+    _l.run()
+
+
+def overfit():
+    _l = Learn(
+        training_size=10,
+        batch_size=1,
+        print_every=1,
+        epochs=100,
+        conv_depth=1,
+    )
+    _l.data_retrieval_command = '''
+        SELECT *
+        FROM elo_ordered_games
+        LIMIT ?'''
+    _l.run()
 
 
 def main():
     Learn(
-        training_size=100000,
+        training_size=1000000,
         batch_size=1000,
-        print_every=10,
-        epochs=50,
+        print_every=100,
+        epochs=1000,
         conv_depth=2,
     ).run()
 
 
 if __name__ == '__main__':
     main()
+    # test()
