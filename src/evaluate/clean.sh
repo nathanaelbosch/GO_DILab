d2ebc67105e5b5b394682b69c1b31427da0a5f7a
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2018-01-14 14:09:24 +0100

    ringmaster has its own reset-command

--- src/evaluate/clean.sh
+++ src/evaluate/clean.sh
@@ -1,5 +1,10 @@
+#!/usr/bin/env bash
+
 rm -r *.games
 rm *.hist
 rm *.log
 rm *.report
 rm *.status
+
+# or use `ringmaster eval_run.ctl reset`
+# (where *.ctl is the control file who's output files you want to delete)
