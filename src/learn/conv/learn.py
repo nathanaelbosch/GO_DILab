69ea73f143038f72a131ec5ee1a05a075ed6815d
Nathanael Bosch <nathanael.bosch@tum.de>
2018-01-19 00:01:12 +0100

    dgx1 parameter update

--- src/learn/conv/learn.py
+++ src/learn/conv/learn.py
@@ -363,8 +363,8 @@ def main():
             'batch_size': 1
         },
         'dgx1': {
-            'training_size': 1000000,
-            'batch_size': 8*1000
+            'training_size': 20000000,
+            'batch_size': 8*2000
         },
         'titanx_16ram_kwargs': {
             'training_size': 2000000,
